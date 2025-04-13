583477152a74ee50e68a663bac74002df4a68d56
tmansuroglu <mansuroglu99@gmail.com>
2025-04-13 23:38:59 +0300

    build: added commitlint to project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ import tseslint from "typescript-eslint"
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended"
 
 export default tseslint.config(
+  { files: ["src/**/*.ts"] },
   eslint.configs.recommended,
   tseslint.configs.strict,
   eslintPluginPrettierRecommended
