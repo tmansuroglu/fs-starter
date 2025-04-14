4fb7a1f6f3bbdfc14b841bb13bf143143e4c5c40
tmansuroglu <mansuroglu99@gmail.com>
2025-04-14 16:30:10 +0300

    feat: added admin page router

--- src/controllers/web/page.controllers.ts
+++ src/controllers/web/page.controllers.ts
@@ -1,5 +1,9 @@
 import { RequestHandler } from "express"
 
 export const renderHomePage: RequestHandler = (req, res) => {
-  res.render("home", { title: "Hello from EJS + TypeScript!" })
+  res.render("home", { title: "This will be my portfolio" })
+}
+
+export const renderAdminPage: RequestHandler = (req, res) => {
+  res.render("admin", { title: "This will be my admin page" })
 }
--- src/routes/web/page.routes.ts
+++ src/routes/web/page.routes.ts
@@ -1,8 +1,13 @@
 import express from "express"
-import { renderHomePage } from "../../controllers/web/page.controllers"
+import {
+  renderAdminPage,
+  renderHomePage,
+} from "../../controllers/web/page.controllers"
 
 const pageRouter = express.Router()
 
 pageRouter.get("/", renderHomePage)
+
+pageRouter.get("/admin", renderAdminPage)
 
 export default pageRouter
