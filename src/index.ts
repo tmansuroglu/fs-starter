10b4e0401733e68cdc805c0967e14ac4de8108f6
tmansuroglu <mansuroglu99@gmail.com>
2025-04-16 19:28:48 +0300

    feat: added rate limiter middleware

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,17 @@ import express from "express"
 import path from "path"
 import pageRouter from "./routes/web/page.routes"
 import { env } from "./config/env"
+import rateLimiterMiddleware from "./middlewares/rate-limiter.middleware"
 
 const app = express()
+
+// TODO: Warning for safety!
+// https://github.com/animir/node-rate-limiter-flexible/wiki/Express-Middleware
 
 app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "views"))
 app.use(express.static(path.join(__dirname, "public")))
-
+app.use(rateLimiterMiddleware)
 app.use("/", pageRouter)
 
 app.listen(env.PORT, () => {
---
+++ src/middlewares/rate-limiter.middleware.ts
@@ -0,0 +1,34 @@
+import { RequestHandler } from "express"
+import Redis from "ioredis"
+import { RateLimiterRedis, RateLimiterRes } from "rate-limiter-flexible"
+
+const redisClient = new Redis({ enableOfflineQueue: false })
+
+const rateLimiter = new RateLimiterRedis({
+  storeClient: redisClient,
+  keyPrefix: "middleware",
+  points: 5,
+  duration: 60,
+  useRedisPackage: true,
+})
+
+const rateLimiterMiddleware: RequestHandler = (req, res, next) => {
+  if (!req.ip) {
+    res.status(400).json({ message: "Invalid request: IP address not found." })
+    return
+  }
+
+  console.log("rate limiter middleware")
+
+  rateLimiter
+    .consume(req.ip)
+    .then(next)
+    .catch((rateLimiterRes: RateLimiterRes) => {
+      res.status(429).json({
+        message: "Too many requests. Try again later.",
+        retryAfter: rateLimiterRes.msBeforeNext / 1000 + "s",
+      })
+    })
+}
+
+export default rateLimiterMiddleware
