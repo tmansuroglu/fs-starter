259ea95fcdd1a62749db354f85a64a323c03dff2
tmansuroglu <mansuroglu99@gmail.com>
2025-04-16 20:31:10 +0300

    feat: added asyncHandler

---
+++ src/utils/async-handler.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { RequestHandler } from "express"
+
+export const asyncHandler =
+  (fn: (...args: any[]) => Promise<any>): RequestHandler =>
+  (req, res, next) => {
+    Promise.resolve(fn(req, res, next)).catch(next)
+  }
