c322767b92cfa02969f44048c1e755a4ba08b2f2
tmansuroglu <mansuroglu99@gmail.com>
2025-04-20 18:12:49 +0300

    feat: added layout

--- src/controllers/web/page.controllers.ts
+++ src/controllers/web/page.controllers.ts
@@ -1,13 +1,13 @@
 import { RequestHandler } from "express"
 
 export const renderHomePage: RequestHandler = (req, res) => {
-  res.render("home", { title: "This will be my portfolio" })
+  res.render("layout", { title: "This will be my portfolio", page: "home" })
 }
 
 export const renderAdminPage: RequestHandler = (req, res) => {
-  res.render("admin", { title: "This will be my admin page" })
+  res.render("layout", { title: "This will be my admin page", page: "admin" })
 }
 
 export const renderLoginPage: RequestHandler = (req, res) => {
-  res.render("login", { title: "Login" })
+  res.render("layout", { title: "Login", page: "login" })
 }
