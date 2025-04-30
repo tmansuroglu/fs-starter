19a84753ff537aa1592183d3252dc03388bd3a09
tmansuroglu <mansuroglu99@gmail.com>
2025-04-30 21:28:27 +0300

    refactor: made getUserByEmail throw error when fails

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -2,7 +2,7 @@ import { db } from "@config/db"
 import { User } from "@generated-prisma"
 
 export const getUserByEmail = async (email: string): Promise<User | null> =>
-  await db.user.findUnique({ where: { email } })
+  await db.user.findUniqueOrThrow({ where: { email } })
 
 export const createUser = async (
   email: string,
